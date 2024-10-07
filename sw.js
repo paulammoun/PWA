8776c7412b50e98f744de474a6832d0e75b69ed8
paulammoun <paul@pcs-lb.com>
2024-10-07 22:29:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // **Template code last revised: 08/30/21
 
-const staticCacheName = 'Amiga-PWA_pwa_cache-v-91.59';
-const dynamicCacheName = 'Amiga-PWA_pwa_dynamic_cache-v-91.59';
+const staticCacheName = 'Amiga-PWA_pwa_cache-v-91.60';
+const dynamicCacheName = 'Amiga-PWA_pwa_dynamic_cache-v-91.60';
 const dynamicCacheSize = 15;
 const offlineFilename = './offline.html';
 const showDebugInfo = true;
